d77e1d42b74a1be354bdbd9e5d49c8a1d0d3d755
mark-kns <mark.kns@gmail.com>
2016-01-17 22:06:12 -0200

    Campo Paid

--- eventex/subscriptions/urls.py
+++ eventex/subscriptions/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from eventex.subscriptions.views import new, detail
 
 urlpatterns = [
-    url(r'^/$', new, name='new'),
+    url(r'^$', new, name='new'),
     url(r'^(\d+)/$', detail, name='detail'),
 ]
