cb9936877c113b6080c42282c2a22213d26aef15
mark-kns <mark.kns@gmail.com>
2016-01-24 16:37:17 -0200

    Managers

--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -28,3 +28,25 @@ class ContactModelTest(TestCase):
         contact = Contact.objects.create(speaker=self.speaker, kind=Contact.EMAIL, value='marcelo@belli.me')
         self.assertEqual('marcelo@belli.me', str(contact))
 
+
+class ContactManagerTest(TestCase):
+    def setUp(self):
+        s = Speaker.objects.create(
+            name='Marcelo Belli',
+            slug='marcelo-belli',
+            photo='http://belli.me/marcelo-pic'
+        )
+
+        s.contact_set.create(kind=Contact.EMAIL, value='marcelo@belli.me')
+        s.contact_set.create(kind=Contact.PHONE, value='41-8803-9137')
+
+    def test_emails(self):
+        qs = Contact.objects.emails()
+        expected = ['marcelo@belli.me']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
+    def test_phones(self):
+        qs = Contact.objects.phones()
+        expected = ['41-8803-9137']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
